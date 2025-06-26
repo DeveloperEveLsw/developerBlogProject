c8adc95fb05929d32d0489758995977c35cb95f0
lws19121 <lws19121@gmail.com>
2025-06-26 22:15:40 +0900

    props 수정

--- src/app/admin/edit/[postId]/page.tsx
+++ src/app/admin/edit/[postId]/page.tsx
@@ -1,8 +1,8 @@
 import PostEditor from '@/components/PostEditor/PostEditor'
 import React from 'react'
 
-const EditPage = async ({ params }: { params: { postId: string } }) => {
-  const { postId } = params
+const EditPage = async ( {params}: {params: Promise<{postId: string}>} ) => {
+  const { postId } = await params
 
   return (
     <PostEditor 
