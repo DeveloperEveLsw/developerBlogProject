1586f1d4296134a5f0dd72c3f24449307dc9719d
dev_leesunwoo <developerevelsw@gmail.com>
2025-06-30 00:53:15 +0900

    md 파싱 html 포함 수정

--- src/utils/markdownHtmlStringify.ts
+++ src/utils/markdownHtmlStringify.ts
@@ -4,13 +4,19 @@ import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import { getHighlighter } from './highlighter';
 import rehypeShiki from '@shikijs/rehype'
+import rehypeRaw from 'rehype-raw'
 
 const markdownHtmlStringify = async ({markdown}: {markdown: string}) => {
     const highlighter = await getHighlighter();
 
     const file = await unified()
-        .use(remarkParse)
-        .use(remarkRehype)
+        .use(remarkParse, {
+            commonmark: true
+        })
+        .use(remarkRehype, {
+            allowDangerousHtml: true
+        })
+        .use(rehypeRaw)
         .use(rehypeShiki, {
           highlighter,
           themes: {
